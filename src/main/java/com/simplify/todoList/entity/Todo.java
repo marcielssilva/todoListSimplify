f62d66a162dddc36d31ba142560dda972651b2fb
Marcielsilva <marciel.ssilva2@gmail.com>
2025-01-18 11:40:53 -0300

    Criação dos contrutores da Entity

---
+++ src/main/java/com/simplify/todoList/entity/Todo.java
@@ -0,0 +1,55 @@
+package com.simplify.todoList.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizada;
+    private int prioridade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizada() {
+        return realizada;
+    }
+
+    public void setRealizada(boolean realizada) {
+        this.realizada = realizada;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
